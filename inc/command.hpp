994cc3f7a8048a1a00e1750903e3f3a8586781ef
Harrand <harrysjh98@gmail.com>
2017-07-07 20:35:40 +0100

    Added new version of Topaz and gitattributes to correctly label the repo as C++

--- inc/command.hpp
+++ inc/command.hpp
@@ -2,6 +2,53 @@
 #define COMMAND_HPP
 #include "world.hpp"
 #include "audio.hpp"
+#include <unordered_set>
+
+class Command
+{
+public:
+	Command(std::string name, std::string description, std::string usage);
+	Command(const Command& copy) = default;
+	Command(Command&& move) = default;
+	Command& operator=(const Command& rhs) = default;
+	~Command() = default;
+	
+	const std::string& getName() const;
+	std::string& getNameR();
+	const std::string& getDescription() const;
+	std::string& getDescriptionR();
+	const std::string& getUsage() const;
+	std::string& getUsageR();
+	std::size_t getExpectedParameterSize() const;
+	
+	virtual bool operator==(const Command& rhs) const;
+	virtual void operator()(const std::vector<std::string>& args) = 0;
+private:
+	std::string name;
+	std::string description;
+	std::string usage;
+};
+
+class CommandExecutor
+{
+public:
+	CommandExecutor() = default;
+	CommandExecutor(const CommandExecutor& copy) = default;
+	CommandExecutor(CommandExecutor&& move) = default;
+	CommandExecutor& operator=(const CommandExecutor& rhs) = default;
+	~CommandExecutor() = default;
+	
+	const std::unordered_set<Command*>& getCommands() const;
+	std::unordered_set<Command*>& getCommandsR();
+	
+	void registerCommand(Command* command);
+	void deregisterCommand(Command* command);
+	void deregisterCommand(const std::string& command_name);
+
+	void operator()(const std::string& name, const std::vector<std::string>& args);
+private:
+	std::unordered_set<Command*> commands;
+};
 
 class CommandCache
 {
